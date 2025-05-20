438c49c66cab06887a7aea128112bb9383252119
vira alfirqotun naziah <vinazalfi@gmail.com>
2025-05-20 11:16:15 +0700

    sistem kasir

--- program_kasir.cpp
+++ program_kasir.cpp
@@ -1,11 +1,7 @@
-# Latihan Struct
-
-## Membuat program Transaksi
-Kode struct Transaksi berisi nama, kuantiti dan harga
-```c
 #include <iostream>
 #include <vector>  // Tambahkan library untuk handle array
 #include <iomanip> // Tambahkan library untuk input/ouput(io) manupulation
+using namespace std;
 
 struct Transaksi
 {
@@ -28,19 +24,19 @@ int main()
     {
         Transaksi item_transaksi; // item_transaksi adalah variabel tipe Transaksi yang akan di input nama, kuantiti dan harga oleh User
 
-        std::string nama_item;           // 1. Buat variabel string untuk menampung input dari User
-        std::cout << "\nNama: ";         // 2. Berikan output ke tampilan terminal, agar user mengetahui apa yang harus dia inputkan
-        std::cin >> nama_item;           // 3. Tampung input user ke dalam variabel yang sudah dibuat sebelumnya [nama_item]
+        string nama_item;                // 1. Buat variabel string untuk menampung input dari User
+        cout << "\nNama: ";              // 2. Berikan output ke tampilan terminal, agar user mengetahui apa yang harus dia inputkan
+        cin >> nama_item;                // 3. Tampung input user ke dalam variabel yang sudah dibuat sebelumnya [nama_item]
         item_transaksi.nama = nama_item; // 4. Masukkan nilai yang sudah ditampung [nama_item] ke variabel [nama] pada struct [item_transaksi.nama]
 
         int kuantiti_item;                       // 1. Buat variabel string untuk menampung input dari User
-        std::cout << "Kuantiti: ";             // 2. Berikan output ke tampilan terminal, agar user mengetahui apa yang harus dia inputkan
-        std::cin >> kuantiti_item;               // 3. Tampung input user ke dalam variabel yang sudah dibuat sebelumnya [kuantiti_item]
+        cout << "Kuantiti: ";                    // 2. Berikan output ke tampilan terminal, agar user mengetahui apa yang harus dia inputkan
+        cin >> kuantiti_item;                    // 3. Tampung input user ke dalam variabel yang sudah dibuat sebelumnya [kuantiti_item]
         item_transaksi.kuantiti = kuantiti_item; // 4. Masukkan nilai yang sudah ditampung [kuantiti_item] ke variabel [kuantiti] pada struct [item_transaksi.kuantiti]
 
         int harga_item;
-        std::cout << "Harga: ";
-        std::cin >> harga_item;
+        cout << "Harga: ";
+        cin >> harga_item;
         item_transaksi.harga = harga_item;
 
         transaksi_array.push_back(item_transaksi); // Setelah semua variabel struct di inputkan oleh User, tambahkan struct yang sudah berisi nilai [item_transaksi] ke dalam array [transaksi_array]
@@ -48,9 +44,9 @@ int main()
 
     // Proses Output tampilan sebanyak data yang berada pada array transaksi [transaksi_array.size()]
     int grand_total = 0; // Buat variabel grand_total untuk menampung nilai dari total tiap harga dari item_transaksi
-    std::cout << "\n========================================================\n";
-    std::cout << std::setw(10) << "Nama" << " | " << std::setw(10) << "Kuantiti" << " | " << std::setw(10) << "Harga" << " | " << std::setw(15) << "Total" << "\n"; // fungsi setw() adalah untuk menentukan width(lebar) atau set width pada terminal
-    std::cout << "--------------------------------------------------------\n";
+    cout << "\n========================================================\n";
+    cout << setw(10) << "Nama" << " | " << setw(10) << "Kuantiti" << " | " << setw(10) << "Harga" << " | " << setw(15) << "Total" << "\n"; // fungsi setw() adalah untuk menentukan width(lebar) atau set width pada terminal
+    cout << "--------------------------------------------------------\n";
 
     for (int i = 0; i < transaksi_array.size(); i++)
     {
@@ -65,4 +61,3 @@ int main()
 
     return 0;
 }
-```
